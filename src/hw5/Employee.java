c016f4ca312cb5e3821cfa14d493827d7f67a0ec
golov <golovenchitc@gmail.com>
2021-02-22 22:47:27 +0300

    ДЗ 5

--- src/hw5/Employee.java
+++ src/hw5/Employee.java
@@ -1,5 +1,28 @@
 package hw5;
 
 public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
 
+
+    public Employee(String fullName, String position, String email, String phone, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void getInfo(){
+        System.out.println(fullName + ", " + position + ", " + email + ", " + phone + ", " + salary + ", " + age);
+    }
 }
